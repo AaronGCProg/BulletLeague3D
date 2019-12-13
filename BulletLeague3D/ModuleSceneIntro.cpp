4c1c2c716a605dfc2106ca66c90974ffb1b6dc17
Aaron Guerrero Cruz <47571598+AaronGCProg@users.noreply.github.com>
2019-12-13 11:16:06 +0100

    Base Car uploaded

--- BulletLeague3D/ModuleSceneIntro.cpp
+++ BulletLeague3D/ModuleSceneIntro.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
+#include "PhysBody3D.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -18,6 +19,7 @@ bool ModuleSceneIntro::Start()
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
+
 	return ret;
 }
 
@@ -29,7 +31,7 @@ bool ModuleSceneIntro::CleanUp()
 	return true;
 }
 
-// Update: draw background
+// Update
 update_status ModuleSceneIntro::Update(float dt)
 {
 	Plane p(0, 1, 0, 0);
@@ -37,5 +39,9 @@ update_status ModuleSceneIntro::Update(float dt)
 	p.Render();
 
 	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
 }
 
