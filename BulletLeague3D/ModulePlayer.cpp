e734f80093837c0ca531b6a2dc90bb9efb409a2a
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-12-17 08:18:46 +0100

    Vehicle is now testeable

--- BulletLeague3D/ModulePlayer.cpp
+++ BulletLeague3D/ModulePlayer.cpp
@@ -331,7 +331,7 @@ btVector3 ModulePlayer::WorldToLocal(float x, float y, float z)
 
 void ModulePlayer::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
 {
-	if (body1->cntType == CNT_VEHICLE_SENSOR && body2->cntType == CNT_GROUND) 
+	if (body1->cntType == CNT_VEHICLE && body2->cntType == CNT_GROUND) 
 	{
 		wallContact = true;
 		secondJump = false;
